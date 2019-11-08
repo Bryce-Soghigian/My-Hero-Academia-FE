1ebb724b176a3a69df3963c03e27c1abb070753f
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-11-08 07:25:12 -0600

    added conditional movie rendering logic

--- src/Components/Navbar.js/Navbar.js
+++ src/Components/Navbar.js/Navbar.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const NavContainer = styled.div`
 height:60px;
 width:100%;
-background-image:linear-gradient(135deg, rgb(116, 44, 58) 0%, rgb(116, 44, 58) 1%,rgb(158, 95, 98) 1%, rgb(158, 95, 98) 49%,rgb(126, 62, 66) 49%, rgb(126, 62, 66) 51%,rgb(200, 36, 63) 51%, rgb(200, 36, 63) 64%,rgb(166, 67, 79) 64%, rgb(166, 67, 79) 68%,rgb(170, 84, 98) 68%, rgb(170, 84, 98) 71%,rgb(113, 43, 50) 71%, rgb(113, 43, 50) 100%);
+background-image:linear-gradient(135deg, rgb(12, 2, 85) 0%, rgb(12, 2, 85) 1%,rgb(12, 2, 85) 1%, rgb(12, 2, 85) 49%,rgb(255, 255, 255) 49%, rgb(126, 62, 66) 51%,rgb(192, 4, 4) 51%, rgb(200, 36, 63) 64%,rgb(255, 255, 255) 29%, rgb(12, 2, 85) 68%,rgb(12, 2, 85) 68%, rgb(12, 2, 85) 71%,rgb(12, 2, 85) 71%, rgb(12, 2, 85) 100%);
 display:flex;
 justify-content:space-evenly;
 align-items:center;
@@ -19,6 +19,7 @@ text-shadow:2px 2px 2px black;
     text-decoration:underline;
     /* transform: rotate(3deg); */
     transform: scale(1.3);   
+    transition:1s;
 }
 
 
--- src/Components/Videos/Video_Home.js
+++ src/Components/Videos/Video_Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function Video_Home() {
+    //side menu to select each of the four seasons
+    
     return (
         <div>
             
