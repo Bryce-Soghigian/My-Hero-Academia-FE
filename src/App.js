02a8cb92ed7010f051b3fb1c56d45fc37fa5099d
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-11-10 11:37:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import {Route} from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 import WikiHome from './Components/Wiki/WikiHome';
 import Navbar from './Components/Navbar.js/Navbar';
 import ChaptersHome from './Components/Chapters/ChaptersHome';
-import MainRender from './Components/Videos/Season_1/MainRender';
+import Test from './Components/Videos/Season_1/Test';
+import SeasonOneRender from './Components/Videos/Season_1/SeasonOneRender';
+import Video_Home from './Components/Videos/Video_Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
   <Route path="/Wiki" component={WikiHome}></Route>
-  <Route path = "/Episodes"/>
-  <Route path="/Season_1" component={MainRender}/>
+  <Route path = "/Episodes" component={Video_Home}/>
+  <Route path="/Season_1" component={SeasonOneRender}/>
   <Route path="/Season_2"/>
   <Route path= '/Season_3'/>
   <Route path="/Season_4"/>
+  <Route path="/test" component={Test}/>
   <Route path="/Chapters" component={ChaptersHome}/>
 
     </div>
